7a46989cc33495ac7696585690bd12827c3c41c9
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-07-17 23:04:22 -0300

    feat: add eslint

--- App.tsx
+++ App.tsx
@@ -1,8 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import SignIn from './src/screens/SignIn';
+import SignIn from './src/screens/SignIn'
 
 export default function App() {
-  return <SignIn />
+	return <SignIn />
 }
