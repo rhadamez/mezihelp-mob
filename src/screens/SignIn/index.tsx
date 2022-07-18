088b7f425f6fded525b0f0a0935a7de9b3be20ca
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-07-17 22:51:17 -0300

    feat: create project and add nativeBase

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function SignIn() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>SignIn!</Text>
       <StatusBar style="auto" />
     </View>
   );
