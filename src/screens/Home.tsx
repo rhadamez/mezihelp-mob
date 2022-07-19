63a9f59861fef2184275501f4a60df056cb4e9cb
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-07-18 23:49:14 -0300

    feat: add FlatList from native-base

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,28 @@
-import { Heading, HStack, IconButton, Text, useTheme, VStack } from 'native-base'
+import { Heading, HStack, IconButton, Text, useTheme, VStack, FlatList } from 'native-base'
 import { SignOut } from 'phosphor-react-native'
+import { useState } from 'react'
 
 import Logo from '../assets/logo_secondary.svg'
 import { Filter } from '../components/Filter'
 
 export function Home() {
 	const { colors } = useTheme()
+	const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open')
+	const [orders, setOrders] = useState([
+		{
+			id: '123',
+			patrimony: '123456',
+			when: '18/07/2022 às 10:00',
+			status: 'open'
+		},
+		{
+			id: '412',
+			patrimony: '321321',
+			when: '05/07/2022 às 11:00',
+			status: 'open'
+		}
+	])
+
 	return (
 		<VStack flex={1} pb={6} bg='gray.700'>
 			<HStack
@@ -28,9 +45,23 @@ export function Home() {
 					<Text color='gray.200'>3</Text>
 				</HStack>
 				<HStack space={3} mb={8}>
-					<Filter title='Em andamento' type='open' />
-					<Filter title='Finalizados' type='closed' />
+					<Filter
+						title='Em andamento'
+						type='open'
+						onPress={() => setStatusSelected('open')}
+						isActive={statusSelected === 'open'} />
+					<Filter
+						title='Finalizados'
+						type='closed'
+						onPress={() => setStatusSelected('closed')}
+						isActive={statusSelected === 'closed'} />
 				</HStack>
+
+				<FlatList
+					data={orders}
+					keyExtractor={item => item.id}
+					renderItem={({ item }) => <Text color='white'>{item.patrimony}</Text>}/>
+
 			</VStack>
 
 
